5ac66e32c955028bdef345774744f9c3e64eea70
luin <i@zihua.li>
2015-06-15 12:18:26 +0800

    Init

---
+++ redis/NSMainSplitViewController.swift
@@ -0,0 +1,12 @@
+//
+//  NSMainSplitViewController.swift
+//  redis
+//
+//  Created by Zihua Li on 6/13/15.
+//  Copyright (c) 2015 Zihua Li. All rights reserved.
+//
+
+import Cocoa
+
+class NSMainSplitViewController: NSSplitViewController {
+}
---
+++ redis/RDWindow.swift
@@ -0,0 +1,12 @@
+//
+//  RDWindow.swift
+//  redis
+//
+//  Created by Zihua Li on 6/14/15.
+//  Copyright (c) 2015 Zihua Li. All rights reserved.
+//
+
+import Cocoa
+
+class RDWindow: NSWindow {
+}
